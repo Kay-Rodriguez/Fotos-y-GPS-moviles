d07de44cc0a845ba93342651f9a2f16fd766f4fc
Alissonviracocha <alissonviracocha602@gmail.com>
2025-10-20 00:20:53 -0500

    galeria de foto

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { PhotoService } from '../services/photo';
 
 @Component({
+  standalone: true,
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  standalone: false,
+  imports: [CommonModule, FormsModule, IonicModule, ExploreContainerComponentModule]
 })
 export class Tab2Page {
+  constructor(public photoService: PhotoService) {}
 
-  constructor() {}
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
+  addPhotoToGallery() {
+    this.photoService.addPhotoToGallery();
+  }
 }
